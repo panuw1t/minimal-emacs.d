42ed2e0c8348a81da112597112ba5354c3310432
doggo <jonh2544za@gmail.com>
2026-01-26 20:59:18 +0700

    first configure

---
+++ pre-early-init.el
@@ -0,0 +1,8 @@
+;;; pre-early-init.el --- pre-early-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(setq debug-on-error t)
+
+;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
+;; NOTE: This must be placed in 'pre-early-init.el'.
+(setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
