898d0daadce9753f78bfc911b9a06413040d5002
doggo <jonh2544za@gmail.com>
2026-01-27 19:37:44 +0700

    add lsp + treesitter

--- pre-early-init.el
+++ pre-early-init.el
@@ -6,3 +6,12 @@
 ;; NOTE: This must be placed in 'pre-early-init.el'.
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+(defun display-startup-time ()
+  "Display the startup time and number of garbage collections."
+  (message "Emacs init loaded in %.2f seconds (Full emacs-startup: %.2fs) with %d garbage collections."
+           (float-time (time-subtract after-init-time before-init-time))
+           (time-to-seconds (time-since before-init-time))
+           gcs-done))
+
+(add-hook 'emacs-startup-hook #'display-startup-time 100)
