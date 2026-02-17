5715c85ec0d371b79106ffa25b582763aec71d79
panuvit sreudomdetsakul <panuvit@asger.co>
2026-02-17 15:45:07 +0700

    add separate toggle-vterm pacakge

--- post-init.el
+++ post-init.el
@@ -12,6 +12,7 @@
   (push "/post-init.el" compile-angel-excluded-files)
   (push "/pre-early-init.el" compile-angel-excluded-files)
   (push "/post-early-init.el" compile-angel-excluded-files)
+  (push "/lisp/toggle-vterm.el" compile-angel-excluded-files)
   (compile-angel-on-load-mode 1))
 
 (use-package autorevert
@@ -541,79 +542,10 @@
                ("C-M-]" . my-vterm-next)
                ("C-M-[" . my-vterm-prev))))
 
-(defun my-vterm-switch (n)
-  "Switch to vterm buffer N, create if it doesn't exist."
-  (let ((name (format "*vterm-%d*" n)))
-    (if (get-buffer name)
-        (switch-to-buffer name)
-      (vterm name))))
-
-(dotimes (i 5)
-  (let ((n (1+ i)))
-    (eval
-     `(defun ,(intern (format "my-vterm-%d" n)) ()
-        (interactive)
-        (my-vterm-switch ,n)))))
-
-(defvar my-last-non-vterm-buffer nil
-  "Stores the last non-vterm buffer.")
-
-(defun my-toggle-vterm ()
-  "Toggle between last vterm and last non-vterm buffer."
-  (interactive)
-  (if (derived-mode-p 'vterm-mode)
-      (when (buffer-live-p my-last-non-vterm-buffer)
-        (switch-to-buffer my-last-non-vterm-buffer))
-    (setq my-last-non-vterm-buffer (current-buffer))
-    (let ((vterm-buffer
-           (seq-find
-            #'my--vterm-buffer-p
-            (buffer-list))))
-      (if vterm-buffer
-          (switch-to-buffer vterm-buffer)
-        (my-vterm-1)))))
-
-(defun my--vterm-buffer-p (buf)
-  "Return non-nil if BUF is a vterm buffer."
-  (with-current-buffer buf
-    (derived-mode-p 'vterm-mode)))
-
-(defun my--vterm-number (buf)
-  "Extract numeric suffix from vterm buffer name."
-  (with-current-buffer buf
-    (if (string-match "\\*vterm-\\([0-9]+\\)\\*" (buffer-name))
-        (string-to-number (match-string 1 (buffer-name)))
-      0)))
-
-(defun my--vterm-buffers ()
-  "Return vterm buffers sorted numerically."
-  (seq-sort
-   (lambda (a b)
-     (< (my--vterm-number a)
-        (my--vterm-number b)))
-   (seq-filter #'my--vterm-buffer-p (buffer-list))))
-
-(defun my-vterm-next ()
-  "Switch to next vterm buffer."
-  (interactive)
-  (let* ((buffers (my--vterm-buffers))
-         (current (current-buffer))
-         (pos (cl-position current buffers)))
-    (when buffers
-      (switch-to-buffer
-       (nth (mod (1+ (or pos -1)) (length buffers))
-            buffers)))))
-
-(defun my-vterm-prev ()
-  "Switch to previous vterm buffer."
-  (interactive)
-  (let* ((buffers (my--vterm-buffers))
-         (current (current-buffer))
-         (pos (cl-position current buffers)))
-    (when buffers
-      (switch-to-buffer
-       (nth (mod (1- (or pos 1)) (length buffers))
-            buffers)))))
+(use-package toggle-vterm
+  :ensure nil
+  :load-path (lambda () (expand-file-name "lisp/" minimal-emacs-user-directory))
+  :after vterm)
 
 ;; (use-package combobulate
 ;;   :custom
